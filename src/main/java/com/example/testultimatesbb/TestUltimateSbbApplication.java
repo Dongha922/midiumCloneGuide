663ac79465cecc6243fdf7a977fcc8ffd8f0956d
dongha922 <kdh4575@naver.com>
2023-12-08 13:35:50 +0900

    세팅

---
+++ src/main/java/com/example/testultimatesbb/TestUltimateSbbApplication.java
@@ -0,0 +1,13 @@
+package com.example.testultimatesbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestUltimateSbbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestUltimateSbbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testultimatesbb/TestUltimateSbbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testultimatesbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestUltimateSbbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
